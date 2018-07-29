7fb0700a8270701338e46787eeef1d9ef04ffef5
Shweta Sinha <shw0072003@yahoo.com>
2018-07-29 16:20:03 +0530

    Shweta

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,10 @@
 import { Injectable } from '@angular/core'
+import { Album } from './album';
+
 import { Http, Response } from '@angular/http'
 import 'rxjs/add/operator/map'
+import { Observable } from 'rxjs/Observable';
+
 
 
 @Injectable()
@@ -9,10 +13,9 @@ export class ProductService {
   	constructor(private _http:Http) { }
 
   	private _albumUrl = '../assets/album.json';
-  	
-  	getAlbum(id:number){
+  	getAlbum(track_id:number): Observable<Album>{
   		return this._http.get(this._albumUrl)
-  				.map((response) => response.json());
+  				.map((response) => <Album>response.json());
   	} 
 
 }
